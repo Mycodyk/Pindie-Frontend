ddb0c1aa84ff4ddcdfa581889d795556ff2c54f5
Максим Федоров <129328321+Mycodyk@users.noreply.github.com>
2024-05-24 15:27:51 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://pindie-back.nomoredomainswork.ru/api";
+export const BASE_URL = "https://pindie-backend-1.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
